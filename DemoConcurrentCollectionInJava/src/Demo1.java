ba91d966f707d3d5fe2d52552bd842ff29b08ad4
sanchit2107 <sanchit2107@gmail.com>
2020-08-09 02:20:26 +0530

    Concurrent Collections in Java

---
+++ DemoConcurrentCollectionInJava/src/Demo1.java
@@ -0,0 +1,11 @@
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		Set<String> set=ConcurrentHashMap.newKeySet();
+		
+	
+	}
+}
